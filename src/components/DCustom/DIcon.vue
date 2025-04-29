32c323fd5c86c8e0a3a25abfb19fdf8b920d1858
Nguyễn Đăng Đức <ducnd@inet.vn>
2025-04-29 13:56:39 +0700

    Update  version 0.0.03 beta

--- src/components/DCustom/DIcon.vue
+++ src/components/DCustom/DIcon.vue
@@ -3,7 +3,7 @@
     inheritAttrs: false,
   });
 
-  const props = defineProps<{
+  defineProps<{
     icon?: string;
     size?: number | string;
     color?: string;
@@ -14,16 +14,6 @@
     (e: 'click'): void;
   }>();
 </script>
-<template>
-  <span
-    v-bind="$attrs"
-    class="d-icon"
-    :class="{ 'd-icon--disabled': disabled }"
-    :style="{ color, fontSize: props.size ? `${props.size}px` : undefined }"
-    @click="emit('click')">
-    <i class="mdi" :class="icon" />
-  </span>
-</template>
 
 <template>
   <i
